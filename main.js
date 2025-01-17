604584319e6eed561ed7e2c59ebb2a2e3f74575a
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2025-01-17 14:08:38 +0000

    sample code - need sign in cert

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
 let mainWindow;
 
 app.on('ready', () => {
@@ -11,4 +12,51 @@ app.on('ready', () => {
   });
 
   mainWindow.loadFile('index.html');
+
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+// autoUpdater.on('update-available', () => {
+//   dialog.showMessageBox({
+//     type: 'info',
+//     title: 'Update Available',
+//     message: 'A new version is available. Downloading now...'
+//   });
+// });
+
+// autoUpdater.on('update-downloaded', () => {
+//   dialog.showMessageBox({
+//     type: 'info',
+//     title: 'Update Ready',
+//     message: 'Update downloaded. It will be installed on restart.',
+//     buttons: ['Restart Now']
+//   }).then(result => {
+//     if (result.response === 0) autoUpdater.quitAndInstall();
+//   });
+// });
+
+// Update events
+autoUpdater.on('checking-for-update', () => {
+  mainWindow.webContents.send('update-status', 'Checking for updates...');
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update-status', 'Update available! Downloading...');
+});
+
+autoUpdater.on('update-not-available', () => {
+  mainWindow.webContents.send('update-status', 'No new updates available.');
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+  mainWindow.webContents.send('update-status', `Downloading: ${Math.round(progressObj.percent)}%`);
+});
+
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update-status', 'Update downloaded! Restarting...');
+  autoUpdater.quitAndInstall();
+});
+
+autoUpdater.on('error', (err) => {
+  mainWindow.webContents.send('update-status', `Error: ${err.message}`);
 });
